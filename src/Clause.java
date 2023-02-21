3bf5a7dee8b5479a5d7a42d051dace66b455f9b9
justinpitera@gmail.com <justinpitera@gmail.com>
2023-02-20 23:47:49 -0500

    tons of changes

--- src/Clause.java
+++ src/Clause.java
@@ -6,14 +6,14 @@ import java.util.List;
 public class Clause
 {
 
-    int numVars; //stores how many variables are in actually in the clause
-    int maxVars; //stores how many possible variables could be in the clause based on the cnf file format
-    LinkedList<Integer> variables = new LinkedList<Integer>();
+    private int numVars; //stores how many variables are in actually in the clause
+    private int numClauses;
+    private LinkedList<Integer> variables = new LinkedList<Integer>();
 
-    public Clause(int numVars, int maxVars, LinkedList<Integer> variables)
+    public Clause(int numVars, int numClauses, LinkedList<Integer> variables)
     {
         this.numVars = numVars;
-        this.numVars = maxVars;
+        this.numClauses = numClauses;
         this.variables = variables;
     }
 
@@ -30,4 +30,14 @@ public class Clause
     public List<Integer> getVariables() {
         return variables;
     }
+
+    // Get literals                   x:  1  2  3  4  5
+    // [4, -3, 2] with 5 vars should be: [0, 1, 0, 1, 0]
+    // In terms of the bruteforcer, if you find a variation in 2^5 where
+    // currentVariation = [0, 1, 0, 1, 0] then the clause is satisfied.
+    // Now do this for all of the clauses in the formula
+    // if all of the clauses are satisfied, the formula is satisfied
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.math.BigInteger;
 import java.util.*;
 
 import java.util.concurrent.*;
@@ -8,11 +9,12 @@ import java.util.concurrent.*;
 //        (2) Add code to test how many ms it takes to bruteforce
 //        Remaining two of these tasks being completed would result in Mission complete.
 
-public class Main {
+public class Main
+{
 
-    static ArrayList<Clause[]> formula = new ArrayList<Clause[]>();
-    static int numClauses = 0; // How many clauses are present in the formula.
-    static int maxNumVars = 0; // The maximum number of variables that can be in a clause at any given time throughout the formula
+    private static ArrayList<Clause[]> formula = new ArrayList<Clause[]>();
+    private static int numClauses = 0; // How many clauses are present in the formula.
+    private static int numVars = 0; // The number of variables that can be in a clause at any given time throughout the formula
 
     public static void main(String[] args) throws InterruptedException, ExecutionException
     {
@@ -30,31 +32,39 @@ public class Main {
     }
 
 
-    public static void loadFile(File cnfFile) throws InterruptedException, ExecutionException {
+    public static void loadFile(File cnfFile) throws InterruptedException, ExecutionException
+    {
         formula.clear(); // Clears the list from the last one
-        try {
+        try
+        {
             // Grab the file from the parameters
             Scanner scanner = new Scanner(cnfFile);
-            while (scanner.hasNextLine()) {
+            while (scanner.hasNextLine())
+            {
                 String line = scanner.nextLine();
-                if (line.startsWith("c")) {
+                if (line.startsWith("c"))
+                {
                     continue;
                 }
 
                 if (line.startsWith("p")) {
-                    String[] args = line.split(" ");
-                    int numVars = Integer.parseInt(args[2]); //variables
-                    int numClauses = Integer.parseInt(args[3]); //clauses
-                    maxNumVars = numVars;
 
-                    for (int i = 0; i < numClauses; i++) {
+                    String[] args = line.split(" ");
+                    numVars = Integer.parseInt(args[2]); //variables
+                    numClauses = Integer.parseInt(args[3]); //clauses
+
+                    for (int i = 0; i < numClauses; i++)
+                    {
                         //initialize a new clause to store the variables
-                        Clause newClause = new Clause(numVars, maxNumVars, new LinkedList<Integer>());
-                        if (scanner.hasNextLine()) {
+                        Clause newClause = new Clause(numVars, numClauses, new LinkedList<Integer>());
+                        if (scanner.hasNextLine())
+                        {
                             String[] varStrings = scanner.nextLine().trim().split(" ");
-                            for (String varString : varStrings) {
+                            for (String varString : varStrings)
+                            {
                                 int var = Integer.parseInt(varString);
-                                if (var != 0) {
+                                if (var != 0)
+                                {
                                     newClause.addVariable(var);
                                 }
                             }
@@ -64,86 +74,83 @@ public class Main {
                     }
                 }
             }
-        } catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e)
+        {
             e.printStackTrace();
         }
         // print the list for the user to see
+
         System.out.println("Working on: " + cnfFile.getPath());
-        System.out.println("Maximum Variables of Variables: " + maxNumVars + ", Number of Clauses: " + formula.size());
-        System.out.println("Amount of variations: " + createVariations(maxNumVars).size());
-        System.out.println("Satisfiable: " + isSatisfiable(formula, createVariations(maxNumVars)));
-        System.out.println("--------------");
+        BigInteger maxVariations = BigInteger.valueOf(2).pow(numVars);
+        System.out.println("Number of Variables: " + numVars + ", Number of Clauses: " + formula.size() + ", Maxiumum Variations: " + maxVariations.toString());
+        System.out.println("Attempting to bruteforce...");
+        bruteForce();
+        System.out.println("------------------------------------");
     }
 
-    public static ArrayList<int[]> createVariations(int inputVars) {
-        int numVars = inputVars;
-        ArrayList<int[]> variations = new ArrayList<>();
 
-        //initialize first variation
-        int[] variableValues = new int[numVars];
 
-        // execute this 2^(inputVars) times
-        for (int i = 0; i < Math.pow(2, numVars); i++) {
-            //add current set of variations to the variation list
-            variations.add(variableValues);
-            // do i have to put the code to test for variation satisfiability here?????????????????????????????????????????????????????????????????????????
-            // get the next variation and set it to vars
-            variableValues = getNextVariation(variableValues);
+/*
+function brute_force_solve(variables, clauses):
+    for assignment in all_possible_assignments(variables):
+        if satisfies(assignment, clauses):
+            return assignment
+    return none
+ */
+
+    public static void bruteForce()
+    {
+        long startTime = System.currentTimeMillis();
+
+        //initialize first variation, all false. but as this goes through the for loop, the final variation should be all true
+        int[] currentVariation = new int[numVars];
+
+        // Generate 2^(numVars) number of variations
+        BigInteger maxVariations = BigInteger.valueOf(2).pow(numVars);
+        for (BigInteger i = BigInteger.ZERO; i.compareTo(maxVariations) < 0; i = i.add(BigInteger.ONE))
+        {
+            // Check ifSatisfiable here but how... check the notes in Clause.java i made. show it to the tutor ask if its right
+
+            currentVariation = getNextVariation(currentVariation);
         }
-
-        return variations;
+        long endTime = System.currentTimeMillis();
+        long elapsedTime = endTime - startTime;
+        System.out.println("Satisfiable: null" ); // This will eventually be implemented
+        System.out.println("Elapsed time: " + elapsedTime + " milliseconds"); // Check, is it supposed to be millis?
     }
+
+
 
     // This method is based off of binary addition to create all the variations for n level of variations.
     // This comes out to 2^n level of variations.
     public static int[] getNextVariation(int[] vars) {
         int[] nextVariation = Arrays.copyOf(vars, vars.length);
         int index = nextVariation.length - 1;
-
         // Find the rightmost 0 value
         while (index >= 0 && nextVariation[index] == 1) {
             index--;
         }
-
         if (index >= 0) {
             // Flip the rightmost 0 value to 1
             nextVariation[index] = 1;
-
             // Change all 1 values to the right of the rightmost 0 to 0
             for (int i = index + 1; i < nextVariation.length; i++) {
                 nextVariation[i] = 0;
             }
         }
-
-
         return nextVariation;
     }
 
-    public static boolean isSatisfiable(ArrayList<Clause[]> formula, ArrayList<int[]> vars) {
-        // initialize formula as unsatisfiable
-        Boolean isSatisfiable = false;
-        for (Clause[] clause : formula) {
-            //                                  x4 = on | x3 = off | 1 = off | 2 = on
-            //                                  assume all other x's in maxVars = off
-            // convert the clause into correct format: [4, -3, -1, 2] , maxVars = 6
-            //                                                = [ 0(x1), 1(x2), 0(x3), 1(x4), 0, 0 ]
-            //                                                 [ 0 1 0 1 0 0 ] and if you find that in the
-            //                                                 variations its satisfiable
-            //
-            // }
 
 
-            // LOGIC
-            // run a for loop through all of the clauses. for each clause in the list
-            // test the vars until you find one that works
-            // if you find one that works flip it to satisfiable and then do the loop again
-            // but on the next clause in the formula.
-            //
-            // if you reach the end of the formula and the boolean isSatisfiable = true.
-            // return true. otherwise, return false.
-        }
-        return true;
-    }
+
+
+
+
+
+
+
+
 }
 
 
